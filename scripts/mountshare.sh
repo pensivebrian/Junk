64919e25baf01bf2f1e6bbabe7b9d6cae13e2c74
Brian O'Neill <pensivebrian@hotmail.com>
2018-07-23 14:25:57 -0700

    Update mountshare.sh

--- scripts/mountshare.sh
+++ scripts/mountshare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sudo apt-get install cifs-utils
-sudo mount.cifs //bro-hb/public ~/winshare -o user=broneill,dom=REDMOND,uid=1000,gid=1000
-sudo mount.cifs //bro-hb/sqlpackage-linux ~/sqlpackage -o user=broneill,dom=REDMOND,uid=1000,gid=1000
+sudo mount.cifs //bro-hk/public ~/winshare -o user=broneill,dom=REDMOND,uid=1000,gid=1000,vers=2.0
+sudo mount.cifs //bro-hk/sqlpackage-linux ~/sqlpackage -o user=broneill,dom=REDMOND,uid=1000,gid=1000,vers=2.0
